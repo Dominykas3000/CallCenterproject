a89d013c39866d2399976196707a2f38c648d137
Dominykas3000 <102905800+Dominykas3000@users.noreply.github.com>
2022-12-12 01:17:47 +0200

    Update scripterino.js

--- scripterino.js
+++ scripterino.js
@@ -1,18 +1,18 @@
 window.onload = function () {
-  console.log("Page loaded");
+  console.log("Page is loaded");
   document.getElementById("submit").addEventListener("click", callTo);
 };
 
 function callTo() {
-  console.log("Will try to get number");
+  console.log("function is called trying to get the number");
   var number = document.getElementById("userPhone").value;
-  console.log("Will ask to call on number " + number);
+  console.log("Users phone number is here and we are thryint to contact : " + number);
   params = "num=" + number;
   params = params.toString();
   var xml = new XMLHttpRequest();
   xml.open("POST", "main.php?num=" + number, true);
   xml.status = function () {
-    console.log("response" + this.responseText);
+    console.log("response of xmlHTTP request is :" + this.responseText);
   };
   xml.send();
 }
